abbf4bb28d97e703aabb30c2e8f2a728d07bf45b
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-07-04 08:54:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,77 @@
-function calculateMinCost(arr) {
-  //your code here
-  let pq = [];
-   
-        // Adding items to the pQueue
-        for (let i = 0; i < n; i++) {
-            pq.push(arr[i]);
-        }   
-           
-        pq.sort(function(a,b){return a-b;});
-         
-        // Initialize result
-        let res = 0;
-   
-        // While size of priority queue
-        // is more than 1
-        while (pq.length > 1) {
-            // Extract shortest two ropes from pq
-            let first = pq.shift();
-            let second = pq.shift();
-   
-            // Connect the ropes: update result
-            // and insert the new rope to pq
-            res += first + second;
-            pq.push(first + second);
-            pq.sort(function(a,b){return a-b;});
+function calculateMinCost() {
+
+    function getTwoSum(arr){
+
+        let compressArr = [];
+    
+        if(arr.length >= 2){
+            let sum = arr[0] + arr[1];
+            compressArr.push(sum);
+    
+            for(let i = 2; i < arr.length; i++){
+                compressArr.push(arr[i]);
+            }
         }
-   
-        return res;
-  
-  
-}  
+    
+        console.log(compressArr+ ` compressedArr`);
+    
+        return compressArr;
+        
+    }
+
+	let form = document.getElementById("form");
+
+    let input = document.getElementById("rope-lengths").value;
+    let splitted = input.split(",");
+    console.log(splitted);
+
+    let minCost = 0; 
+
+    for(let x in splitted){
+        splitted[x] = Number(splitted[x]);
+    }
+
+    splitted.sort(function(a,b){
+        return a - b;
+    });
+
+    console.log(splitted + ` ----> splitted`);
+
+    if(splitted.length == 1){
+        minCost = splitted[0];
+    }else{
+
+        while(splitted.length > 1){
+            let receivedArr = getTwoSum(splitted);
+            // let receivedSum = getSum(splitted);
+            
+
+            console.log(receivedArr + `receivedArr`);
+
+            minCost = minCost + receivedArr[0];
+
+            console.log(minCost + `minCost`);
+
+            let rearrangedArr = receivedArr.sort(function(a, b){
+                return a - b;
+            });
+
+            console.log(rearrangedArr + `rearrangedArr`)
+
+            splitted = rearrangedArr;
+
+            console.log(splitted + `splitted`);
+        }
+        
+    }
+
+
+    console.log(`final ` + minCost);
+    
+
+    let result = document.getElementById("result");
+    result.textContent = minCost;
+
+
+    
+  }  
